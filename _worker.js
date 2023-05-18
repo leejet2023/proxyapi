8237ad57b2e86239b31562fac7fdf5eec4922ed9
leejet2023 <133938390+leejet2023@users.noreply.github.com>
2023-05-18 11:57:05 +0800

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -1,12 +1,12 @@
-export default {
-  async fetch(request,env) {
-    const url = new URL(request.ur1);
-    url.host = "api.openai.com";
-    return fetch(url,{
-      headers: request.headers ,
-      method: request.method,
-      body: request.body,
-      redirect:'follow'
-    };
-  }
+export default{
+	async fetch(request,env){
+		const url = new URL(request.url);
+		url.host = "api.openai.com";
+		return fetch(url,{
+			headers:request.headers,
+			method:request.method,
+			body:request.body,
+			redirect:'follow'
+		});
+	}
 }
